2922eff53cc4c05263f171cf1eb079666a9abdee
Modest J. <modestjuarez@gmail.com>
2024-04-14 15:17:49 -0700

    Edited the line for nurses homepage declaration

--- src/application/StaffLogin.java
+++ src/application/StaffLogin.java
@@ -107,7 +107,7 @@ class StaffLogin extends Stage {
         			
         			//added code to pop up the nurse homepage when nurse succesfully logs in
         			if(userType == "Nurse") {
-        				new NursesHomepage(username);
+        				new NursesHomepage(username).show();
         	        	//homepage.show();
         			}else if (userType == "Doctor"){
         				//pop up the doctors page when doctor succesfully logs in
